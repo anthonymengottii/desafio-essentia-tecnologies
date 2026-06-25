e0c3e2a2cce45d459e32d4bcf91219343b1ec77e
Anthony Mengotti <anthonymengottii@gmail.com>
2026-06-25 00:40:28 -0300

    style(frontend): refinamento visual (tema, formulários, lista de tarefas)

--- frontend/src/app/features/tasks/task-list/task-list.component.ts
+++ frontend/src/app/features/tasks/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../../core/task.service';
@@ -14,6 +14,7 @@ export class TaskListComponent implements OnInit {
   private taskService = inject(TaskService);
 
   tasks = signal<Task[]>([]);
+  pendingCount = computed(() => this.tasks().filter((t) => !t.completed).length);
   loading = signal(false);
   error = signal('');
 
