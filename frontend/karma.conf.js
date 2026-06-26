bba97f185492eb531cc0f739be23e4aa70638bb8
Anthony Mengotti <anthonymengottii@gmail.com>
2026-06-25 23:00:43 -0300

    ci: usa ChromeHeadless com --no-sandbox para os testes no runner

---
+++ frontend/karma.conf.js
@@ -0,0 +1,23 @@
+// Karma config — necessária para o launcher ChromeHeadlessNoSandbox usado no CI.
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
+  });
+};
